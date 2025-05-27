34d821fd3fb89d330f405f52b7d593e4f1c11e6c
KKien2005 <suprominhkien@gmail.com>
2025-05-28 00:27:15 +0700

    IC in Dashboard

--- Csharp-Entity-Store-Management/Admin/frmDashboard.Designer.cs
+++ Csharp-Entity-Store-Management/Admin/frmDashboard.Designer.cs
@@ -20,5 +20,30 @@ namespace Csharp_Entity_Store_Management
             }
             base.Dispose(disposing);
         }
-    }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnSuppilerManagement = new System.Windows.Forms.Button();
+            this.btnProductManagement = new System.Windows.Forms.Button();
+            this.btnStockInManagement = new System.Windows.Forms.Button();
+            this.btnOrderManagement = new System.Windows.Forms.Button();
+            this.btnCategoryManagement = new System.Windows.Forms.Button();
+            this.btnAccountManagement = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+        }
 }
